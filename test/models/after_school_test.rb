5883f42def3fd8e33901c4c22b80c1c70ff47457
Stefan Dasbach <StefanDasbach@gmail.com>
2015-02-27 13:26:09 -0500

    Pass initial presence validations: AfterSchool, Enrichment, FieldTrip, Location, and Program Models

--- test/models/after_school_test.rb
+++ test/models/after_school_test.rb
@@ -4,11 +4,11 @@ class AfterSchoolTest < ActiveSupport::TestCase
   should belong_to(:program)
 
   should validate_presence_of(:date)
-  #should validate_date(:date)
   should validate_presence_of(:time_in)
   should validate_presence_of(:time_out)
-  #should validate_format_of times
-  #should validate timeout is after timein
   should validate_numericality_of(:total_hours)
   # Test for >= 0
+  #should validate_format_of times
+  #should validate timeout is after timein
+  #should validate_date(:date)
 end
