f1f7a83fb3037838d3a4693717c47cf661b71ff9
supernovamaniac <alterprist@gmail.com>
2015-02-27 02:39:08 -0500

    finished initial model for instructors and users - Alex Kim

---
+++ app/models/instructor.rb
@@ -0,0 +1,27 @@
+class Instructor < ActiveRecord::Base
+
+	has_one :user
+	has_many :assignments
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates_presence_of :user_id
+
+	scope :alphabetical, -> { order('last_name', 'first_name')}
+
+	private
+
+	def name
+		"#{self.last_name}, #{self.first_name}"
+	end
+
+	def proper_name
+		"#{self.first_name} #{self.last_name}"
+	end
+
+	def is_active
+		return true if self.user.active
+		return false
+	end
+end
+
