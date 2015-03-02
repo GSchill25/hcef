6842b37aee06695a64c64ea8563094041bf0eb74
Graham Schilling <gschilli@andrew.cmu.edu>
2015-03-01 19:01:07 -0500

    added date of birth field to guardians and added to guardian form as well

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227013915) do
+ActiveRecord::Schema.define(version: 20150301235256) do
 
   create_table "after_schools", force: true do |t|
     t.date     "date"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20150227013915) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date_of_birth"
   end
 
   create_table "locations", force: true do |t|
