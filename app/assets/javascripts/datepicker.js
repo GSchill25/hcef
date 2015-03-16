a1967eea2f4e1f7764c5643400d1e712bca7bf53
Stefan Dasbach <StefanDasbach@gmail.com>
2015-03-15 22:24:52 -0400

    Allow selection of a date for AfterSchool table

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -2,3 +2,9 @@ $(".programs.new").ready(function() {
    $('.datepick').datepicker({ });
    $("#start_date").datepicker( "setDate" , new Date() );
 });
+
+
+$(".after_schools.new").ready(function() {
+   $('.datepick').datepicker({ });
+   $("#date").datepicker( "setDate" , new Date() );
+});
