a4a0fea6ce67697714b98dd2c29e23e06b9068be
Graham Schilling <gschilli@andrew.cmu.edu>
2015-03-22 21:22:32 -0400

    fixed problem with select dropdowns not displaying

--- app/assets/javascripts/material_select.js
+++ app/assets/javascripts/material_select.js
@@ -2,7 +2,23 @@ $(".children.new").ready(function() {
    $('select').material_select();
 });
 
+$(".children.edit").ready(function() {
+   $('select').material_select();
+});
+
 
 $(".instructors.new").ready(function() {
+   $('select').material_select();
+});
+
+$(".instructors.edit").ready(function() {
+   $('select').material_select();
+});
+
+$(".programs.new").ready(function() {
+   $('select').material_select();
+});
+
+$(".programs.edit").ready(function() {
    $('select').material_select();
 });
