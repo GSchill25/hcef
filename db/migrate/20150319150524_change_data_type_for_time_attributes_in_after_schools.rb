c49f77ac672dd2fb4b1f04df2e4d3cf945f36986
Stefan Dasbach <StefanDasbach@gmail.com>
2015-03-24 11:27:45 -0400

    Allow heroku's pg database to change data types of after_schools columns from string to integer

--- db/migrate/20150319150524_change_data_type_for_time_attributes_in_after_schools.rb
+++ db/migrate/20150319150524_change_data_type_for_time_attributes_in_after_schools.rb
@@ -1,14 +1,8 @@
 class ChangeDataTypeForTimeAttributesInAfterSchools < ActiveRecord::Migration
-  def up
+  def change
     change_column :after_schools, :homework_time, 'integer USING CAST(homework_time AS integer)'
     change_column :after_schools, :literacy_time, 'integer USING CAST(literacy_time AS integer)'
     change_column :after_schools, :technology_time, 'integer USING CAST(technology_time AS integer)'
     change_column :after_schools, :reading_specialist_time, 'integer USING CAST(reading_specialist_time AS integer)'
-  end
-  def down
-    change_column :after_schools, :homework_time, :time
-    change_column :after_schools, :literacy_time, :time
-    change_column :after_schools, :technology_time, :time
-    change_column :after_schools, :reading_specialist_time, :time
   end
 end
