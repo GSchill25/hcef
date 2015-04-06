1001e8e04bfd437157b007365304cf536bb8e130
Alex Kim <alterprist@gmail.com>
2015-04-05 20:10:48 -0400

    added few tests

--- test/models/after_school_test.rb
+++ test/models/after_school_test.rb
@@ -2,13 +2,16 @@ require 'test_helper'
 
 class AfterSchoolTest < ActiveSupport::TestCase
   should belong_to(:program)
+  should belong_to(:child)
 
   should validate_presence_of(:date)
   should validate_presence_of(:time_in)
   should validate_presence_of(:time_out)
   should validate_numericality_of(:total_hours)
-  # Test for >= 0
-  #should validate_format_of times
-  #should validate timeout is after timein
-  #should validate_date(:date)
+
+  should_not allow_value(nil).for(:date)
+  
+  #ascending
+  #descending
+  #for_program
 end
