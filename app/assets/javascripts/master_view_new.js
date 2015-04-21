b7f2a212a6b6e0b30b81fda098fdd18ba7a4d999
Stefan Dasbach <StefanDasbach@gmail.com>
2015-04-21 00:45:03 -0400

    Display names of programs upon submission of MasterView form

--- app/assets/javascripts/master_view_new.js
+++ app/assets/javascripts/master_view_new.js
@@ -59,15 +59,63 @@ function submit() {
     var table_body = document.getElementById('table_body');
     table_body.innerHTML = '';
     var locationObjects = response.locations
-    if (response && locationObjects) {
-      for (var i = 0; i < locationObjects.length; i++) {
-        var tr = document.createElement('tr');
-        var td = document.createElement('td');
-        td.innerHTML = locationObjects[i].name;
-        tr.appendChild(td);
-        table_body.appendChild(tr);
-      }
+    var after_schools = response.after_schools
+    var enrichments = response.enrichments
+    var field_trips = response.field_trips
+    if (response) {
+      populateTables(after_schools, enrichments, field_trips);
     }
   });
 return false;
+}
+
+function populateTables(after_schools, enrichments, field_trips) {
+  /* Gather all table bodies */
+  var after_schools_table = document.getElementById('after_schools').getElementsByTagName('tbody')[0];
+  var enrichments_table = document.getElementById('enrichments').getElementsByTagName('tbody')[0];
+  var field_trips_table = document.getElementById('field_trips').getElementsByTagName('tbody')[0];
+
+  /* Remove all previous table body data */
+  enrichments_table.innerHTML = '';
+  after_schools_table.innerHTML = '';
+  field_trips_table.innerHTML = '';
+
+  if (after_schools) {
+      for (var i = 0; i < after_schools.length; i++) {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        var a = document.createElement('a');
+        a.setAttribute('href','/programs/' + after_schools[i].id);
+        a.innerHTML = after_schools[i].name;
+        td.appendChild(a);
+        tr.appendChild(td);
+        after_schools_table.appendChild(tr);
+      }
+  }
+
+  if (enrichments) {
+      for (var i = 0; i < enrichments.length; i++) {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        var a = document.createElement('a');
+        a.setAttribute('href','/programs/' + enrichments[i].id);
+        a.innerHTML = enrichments[i].name;
+        td.appendChild(a);
+        tr.appendChild(td);
+        enrichments_table.appendChild(tr);
+      }
+  }
+
+  if (field_trips) {
+      for (var i = 0; i < field_trips.length; i++) {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        var a = document.createElement('a');
+        a.setAttribute('href','/programs/' + field_trips[i].id);
+        a.innerHTML = field_trips[i].name;
+        td.appendChild(a);
+        tr.appendChild(td);
+        field_trips_table.appendChild(tr);
+      }
+  }
 }
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -10,12 +10,31 @@ class StaticController < ApplicationController
   end
 
   def master_view_submit
-    location_ids = params["locations"]
-    if !location_ids.nil? 
-      location_ids.map!(&:to_i)
-      locations = Location.all.alphabetical.reject!.with_index { |l, i| !location_ids.include? i }
+    location_indices = params["locations"]
+    if !location_indices.nil? 
+      # Cast location_indices to integers
+      location_indices.map!(&:to_i)
+      # Rejects all locations that are in the ith index spot of the array
+      allLocations = Location.all.alphabetical
+      locations = allLocations.reject!.with_index { |l, i| !location_indices.include? i }
+      # The case where all locations are selected, so reject! will return nil since there is no change from allLocaitons
+      locations = allLocations if locations.nil?
+      after_schools = []
+      enrichments = []
+      field_trips = []
+      locations.each do |l|
+        l.programs.each do |p|
+          if p.program_type == 'after_school'
+            after_schools << p
+          elsif p.program_type == 'enrichment'
+            enrichments << p
+          elsif p.program_type == 'field_trip'
+            field_trips << p
+          end
+        end
+      end
     end
-    render :json => {locations: locations} 
+    render :json => {locations: locations, after_schools: after_schools, enrichments: enrichments, field_trips: field_trips} 
   end
 
   def create_child
