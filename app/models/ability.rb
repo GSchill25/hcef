fe7c184ea607abce748de05a978c97987258cbde
Alex Kim <alterprist@gmail.com>
2015-04-22 14:17:47 -0400

    ability.rb fixed with new schema

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,74 +9,63 @@ class Ability
         can :manage, :all
     elsif user.role? :instructor
         #afterschool
-        #this next line of code does not work; figure out why
-        can :create, AfterSchool
-        can :manage, AfterSchool
+        can :manage, AfterSchool do |afterschool|
+            user_afterschools = user.instructor.locations.map{|c| c.programs.map{|d| d.after_schools.map(&:id)}.flatten}.flatten
+            user_afterschools.include? afterschool.id
+        end
 
-       # can :manage, AfterSchool do |afterschool|
-       #     afterschool_instructors = user.instructor.programs.map{|c| c.after_schools.map(&:id)}
-       #     afterschool_instructors.include? after_school.id
-       #     locations_instructors = user.instructor.locations.map{|c| c.programs }
-       #     for locations_instructors do |afterschool|
-       #     end                
-       # end
-        
-        #can :read, After_school
-        #can :update, After_school do |afterschool|
-            #afterschool_ins = afterschool.program.instructors.map(&:id)
-        #end
-
-        #assignment
-        
         #child
         can :create, Child
         can :manage, Child do |child|
+            user_child = user.instructor.locations.map{|c| c.children.map(&:id)}.flatten
+            user_child.include? child.id
         end
-        #keep in mind this may change
         
         #enrichment
-        can :create, Enrichment
         can :manage, Enrichment do |enrichment|
+            user_enrichments = user.instructor.locations.map{|c| c.programs.map{|d| d.after_schools.map(&:id)}.flatten}.flatten
+            user_enrichments.include? afterschool.id
         end
         
-        #enrollment
-        
         #field_trip
-        can :create, FieldTrip
         can :manage, FieldTrip do |field_trip|
+            user_field_trips = user.instructor.locations.map{|c| c.programs.map{|d| d.field_trips.map(&:id)}.flatten}.flatten
+            user_field_trips.include? field_trip.id
         end
         
         #guardian
+        can :create, Guardian
         can :manage, Guardian do |guardian|
+            user_location = user.instructor.locations.map{|c| c.guardians.map(&:id)}.flatten
+            user_location.include? guardian.id
         end
-        #make sure instructors can only see guardians linked to the child
-        #DO THIS
         
         #instructor
-        can :read, Instructor
+        can :read, Instructor do |instructor|
+            user_location = user.instructor.locations.map(&:id)
+            instructor_location = instructor.locations.map(&:id)
+            (user_location & guardian_location).any?            
+        end
+
         can :update, Instructor do |instructor|
             instructor.id == user.instructor_id
         end
 
         #location
         can :read, Location do |location|
-            instructor_location = user.instructor.locations.map(&:id)
-            instructor_location.include? location.id
+            user_location = user.instructor.locations.map(&:id)
+            user_location.include? location.id
         end
-
-        #can :create, Location
-        #can :manage, Location do |location|
-        #    program_instructors = user.instructor.programs.map{|c| c.location.id}
-        #    program_instructors.include? location.id
-        #end
-
         #sub_location
+        can :read, SubLocation do |sub_location|
+            user_sub_location = user.instructor.locations.map{|c| c.sub_locations.map(&:id)}.flatten
+            user_sub_location.include? sub_location.id
+        end
 
         #program
         can :read, Program do |program|
-            location_instructors = user.instructor.locations.map(&:id)
-            program_location = program.location.id
-            location_instructors.include? program.location.id
+            user_programs = user.instructor.locations.map{|c| c.programs.map(&:id)}.flatten
+            user_programs.include? program.id
         end
 
         #provider
@@ -84,13 +73,11 @@ class Ability
         
         #school
         can :manage, School
-        
+
         #user
         can :update, User do |current_user|
             current_user.id == user.id
         end
-    elsif
-        can :manage, :all
     #elsif user.role? :guardian
     #    can :manage, Child do |child|
     #        child.guardian.id == user.guardian_id
