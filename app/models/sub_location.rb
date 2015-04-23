e417439bdd0e42a247ed6ccbd4dd46e50d05bc36
Stefan Dasbach <StefanDasbach@gmail.com>
2015-04-23 15:13:20 -0400

    Disable some presence validations on Location, Program, and SubLocation models

--- app/models/sub_location.rb
+++ app/models/sub_location.rb
@@ -3,13 +3,13 @@ class SubLocation < ActiveRecord::Base
   
   # Validations
   validates_presence_of :name
-  validates_presence_of :address_line_one
-  validates_presence_of :city
-  validates_presence_of :state
-  validates_presence_of :zip
-  validates_presence_of :phone
-  validates_format_of :zip, with: /\A(\d{5}(-\d{4})?)\z/, message: "should be in the form 12345 or 12345-1234"
-  validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be in the form 412 456 7890"
+  #validates_presence_of :address_line_one
+  #validates_presence_of :city
+  #validates_presence_of :state
+  #validates_presence_of :zip
+  #validates_presence_of :phone
+  #validates_format_of :zip, with: /\A(\d{5}(-\d{4})?)\z/, message: "should be in the form 12345 or 12345-1234"
+  #validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be in the form 412 456 7890"
 
   # Scopes
   scope :alphabetical, -> { order("name") }
