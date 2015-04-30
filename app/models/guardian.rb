dbf2d48cb898766851e488d8ae1e864f2d34ca04
Graham Schilling <gschilli@andrew.cmu.edu>
2015-04-30 12:17:27 -0400

    minor changes to validations and forms

--- app/models/guardian.rb
+++ app/models/guardian.rb
@@ -9,8 +9,8 @@ class Guardian < ActiveRecord::Base
 	#validations
 	validates_presence_of :first_name, :last_name, :phone, :email
 	validates_date :date_of_birth, :before => lambda { Date.today }, on: :create
-	validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-. ]\d{4})\z/, message: "should be 10 digits"
-  validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info|name))\z/i, message: "is not a valid format"
+	validates_format_of :phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-. ]\d{4})\z/, message: "should be 10 digits", :allow_blank => true
+  	validates_format_of :email, with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info|name))\z/i, message: "is not a valid format", :allow_blank => true
 
 	#scopes
 	scope :alphabetical, -> { order('last_name', 'first_name')}
