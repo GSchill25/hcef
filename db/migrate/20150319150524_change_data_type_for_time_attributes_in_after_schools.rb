64d4050f3013f5fe496a1bed2b810e0ea6a41517
Martin Sherbondy <martin.sherbondy@gmail.com>
2015-09-11 01:16:00 -0400

    Fix another broken migration

--- db/migrate/20150319150524_change_data_type_for_time_attributes_in_after_schools.rb
+++ db/migrate/20150319150524_change_data_type_for_time_attributes_in_after_schools.rb
@@ -1,8 +1,8 @@
 class ChangeDataTypeForTimeAttributesInAfterSchools < ActiveRecord::Migration
   def change
-    change_column :after_schools, :homework_time, 'integer USING CAST(extract(epoch from homework_time) AS integer)'
-    change_column :after_schools, :literacy_time, 'integer USING CAST(extract(epoch from literacy_time) AS integer)'
-    change_column :after_schools, :technology_time, 'integer USING CAST(extract(epoch from technology_time) AS integer)'
-    change_column :after_schools, :reading_specialist_time, 'integer USING CAST(extract(epoch from reading_specialist_time) AS integer)'
+    change_column :after_schools, :homework_time, :integer #'integer USING CAST(extract(epoch from homework_time) AS integer)'
+    change_column :after_schools, :literacy_time, :integer #'integer USING CAST(extract(epoch from literacy_time) AS integer)'
+    change_column :after_schools, :technology_time, :integer #'integer USING CAST(extract(epoch from technology_time) AS integer)'
+    change_column :after_schools, :reading_specialist_time, :integer #'integer USING CAST(extract(epoch from reading_specialist_time) AS integer)'
   end
 end
